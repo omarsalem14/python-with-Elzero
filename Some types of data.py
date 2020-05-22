2872baa99d4132ca049d111501e2c8dd11774319
omar <omsalem147@gmail.com>
2020-05-22 14:46:02 +0400

    This is the first lesson

---
+++ Some types of data.py
@@ -0,0 +1,29 @@
+# ------------------------------------------------------
+# type()Sowing the kind of data that i'am deeling with
+# all data in python is object
+# ------------------------------------------------------
+
+print(type(10))  # int => Integer
+print(type(100))  # int => Integer
+print(type(-10))  # int => Integer
+
+
+print(type(100.24))  # float => Floating Point Number
+print(type(2650.222))  # float => Floating Point Number
+print(type(-100.2546))  # float => Floating Point Number
+
+
+print(type("Hello,Python"))  # str => String
+
+
+print(type([1, 2, 3, 4, 5, 6, 7, 8, 9]))  # list => List
+
+
+print(type((1, 2, 3, 4, 5, 6, 7, 8, 9)))  # tuple => Tuple
+
+
+print({"one": 1, "two": 2, "three": 3})  # dict => Dictionary
+
+
+print(2 == 2)  # bool =>Boolean Value:True
+print(2 == 4)  # bool =>Boolean Value:False
